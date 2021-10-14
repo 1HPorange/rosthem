21eda7fcf259d7704e893c28953eca59d9e5cc3a
Markus Webel <markuswebel@gmail.com>
2021-10-14 23:41:42 +0200

    re-exported rosthem dto crate

--- rosthem/src/lib.rs
+++ rosthem/src/lib.rs
@@ -4,3 +4,5 @@ pub use rosthem::{
     error::CoapError, session_ext::CoapSessionExt, Coap, CoapAddress, CoapContext, CoapLogLevel,
     CoapMethod, CoapOptList, CoapPduBuilder, CoapSession, CoapToken, CoapUri,
 };
+
+pub use rosthem_dto;
